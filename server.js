3c185d28ed4af25085796f539dbdc7fa0b7d51f1
Marco Salerno <masalern84@gmail.com>
2021-04-10 07:26:42 -0600

    Express Router

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const campsiteRouter = require("./routes/campsiteRouter");
 
 const hostname = "localhost";
 const port = 3000;
@@ -9,32 +10,9 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
+app.use("/campsites", campsiteRouter);
 
-app.all("/campsites", (req, res, next) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  next();
-});
-
-app.get("/campsites", (req, res) => {
-  res.end("will send all the campsites to you");
-});
-
-app.post("/campsites", (req, res) => {
-  res.end(
-    `Will add the campsite: ${req.body.name} with description: ${req.body.description}`
-  );
-});
-
-app.put("/campsites", (req, res) => {
-  res.statusCode = 403;
-  res.end("PUT operation not supported on /campsites");
-});
-
-app.delete("/campsites", (req, res) => {
-  res.end("Deleting all campsites");
-});
-
+/*
 app.get("/campsites/:campsiteId", (req, res) => {
   res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
 });
@@ -61,6 +39,7 @@ app.use((req, res) => {
   res.setHeader("Content-Type", "text/html");
   res.end("<html><body><h1>This is an Express Server</h1></body></html>");
 });
+*/
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
